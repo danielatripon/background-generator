c706662b5e141052f13c2a5dad953a35123fa68e
Daniela Tripon <daniela.tripon29@gmail.com>
2021-11-18 13:57:31 +0000

    playing with npm, load dash, package.json

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+var _ = require("lodash");
+// Load the core build.
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log("answer: ", _.without(array, 3));
+
+console.log(_);
 const object = {
   name: "Sally",
 };
